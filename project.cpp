d3aba74fbb1b4110dd68e109ad5fb8bb022fde0e
rutayanp <rutayanp@gmail.com>
2013-10-01 00:52:20 -0700

    Flow working till ASU....MLU yet to be implemented

--- project.cpp
+++ project.cpp
@@ -21,43 +21,43 @@ void imemory();
 void regfile();
 int atoint(string);
 
+struct ib_packet
+{
+    string ib_instruction;
+    int ib_flag;
+};
+
 class decode
 {
-    string instruction;
-    struct ib_packet
-    {
-        string ib_instruction;
-        int ib_flag;
-    };
+    ib_packet pre_buffer;
 
 public:
-    string fetch(int pc)
+    void fetch(int pc)
     {
-        instruction = imem[pc];
-        return instruction;
+       pre_buffer.ib_instruction = imem[pc];
     }
 
-    ib_packet decod(string instruction)
+    void decod()
     {
-        ib_packet pre_buffer;
+        //ib_packet pre_buffer;
         stringstream src;
-        string ib_instruction;
+        //string ib_instruction;
         string opcode, dest, src1, src2;
         int operands[2], ib_flag = 0;
 
-        opcode = instruction.substr(1,3);
-        dest = instruction.substr(5,2);
-        operands[0] = (int)instruction.at(9) - 48; //get the int value of operands
-        operands[1] = (int)instruction.at(12) - 48;//get the int value of operands
-
+        opcode = pre_buffer.ib_instruction.substr(1,3);
+        dest = pre_buffer.ib_instruction.substr(5,2);
+        operands[0] = (int)pre_buffer.ib_instruction.at(9) - 48; //get the int value of operands
+        operands[1] = (int)pre_buffer.ib_instruction.at(12) - 48;//get the int value of operands
+        
         src << rf[operands[0]][0];
         src1 = src.str();
         src.str(string());//clear the string stream
-
+    
         src << rf[operands[1]][0];
         src2 = src.str();
         src.str(string());//clear the string stream
-
+        
         if (rf[operands[0]][1] == 0 && rf[operands[1]][1] == 0){
             pre_buffer.ib_instruction = "<" + opcode + "," + dest + "," + src1 + "," + src2 + ">";
             pre_buffer.ib_flag = 1;
@@ -65,29 +65,160 @@ public:
             pre_buffer.ib_flag = 0;
             pre_buffer.ib_instruction = "";
         }
+        //return pre_buffer;
+    }
+
+    ib_packet decode_to_ib(int pc)
+    {
+        fetch(pc);
+        decod();
         return pre_buffer;
     }
 
-    void decode_to_ib(int pc)
+    //WRITE IM DISPLAY
+};
+
+class issue
+{
+private:
+    string sb_pre_buf;
+    string mb_pre_buf;
+    int issue1_flag;
+    int issue2_flag;
+public:
+    
+    void issue_route(string instruction)
     {
-        ib_packet pre_buffer;
-        pre_buffer.ib_instruction = fetch(pc);
-        pre_buffer = decod(pre_buffer.ib_instruction);
-        if(DEBUG > 2){
-            cout << "=========" << endl;
-            cout << "ib_instruction : " << pre_buffer.ib_instruction << endl;
+        issue2_flag = instruction.compare(1,3,"MUL");
+        if(issue2_flag != 0)
+            issue2_flag = instruction.compare(1,3,"DIV");
+
+        issue1_flag = instruction.compare(1,3,"ADD");
+        if(issue1_flag != 0)
+            issue1_flag = instruction.compare(1,3,"SUB");
+    
+        if(issue1_flag == 0)
+            issue1_flag = 1;
+        if(issue2_flag == 0)
+            issue2_flag = 1;
+    }
+    
+    void ib_sb(ib_packet post_ib_buffer)
+    {
+        if(issue1_flag == 1){
+            sb_pre_buf = post_ib_buffer.ib_instruction;
+        } else {
+            sb_pre_buf = "";
         }
+    }
+
+    void ib_mb(ib_packet post_ib_buffer)
+    {
+        if(issue2_flag == 1){
+            mb_pre_buf = post_ib_buffer.ib_instruction;
+        } else {
+            mb_pre_buf = "";
+        }
+    }
+
+    vector<string> ib_2_mb_and_sb (ib_packet post_ib_buf)
+    {
+        vector<string> sb_mb_buf;
+        
+        issue_route(post_ib_buf.ib_instruction);
+        
+        ib_sb(post_ib_buf);
+        ib_mb(post_ib_buf);
+        
+        sb_mb_buf.push_back(sb_pre_buf);
+        sb_mb_buf.push_back(mb_pre_buf);
+
+        return sb_mb_buf;
+    }
+};
+
+class ASU
+{
+private: 
+    int result;
+    int operation; //(0)no-op & (1)ADD & (2)SUBTRACTION
+    int src1, src2;
+    string src1_str, src2_str;
+    int dest;
+    int i;
+public:
+
+    void decode(string instruction)
+    {
+        if(instruction.length() > 3){
+            if(instruction.compare(1,3,"ADD") == 0)
+                operation = 1;
+            else if(instruction.compare(1,3,"SUB") == 0)
+                operation = 2;
+        
+        for (i = 8; instruction.at(i) != ','; i++){
+            src1_str.push_back(instruction.at(i));
+        }
+
+        for (i = ++i; instruction.at(i) != '>';i++){
+            src2_str.push_back(instruction.at(i));
+        }    
+        src1 = atoint(src1_str);
+        src2 = atoint(src2_str);
+        dest = atoint(instruction.substr(6,1));
+        //cout << "src1 : " << src1 << ", "<< "src2 : " << src2 <<", " << dest <<endl; 
+        } else {
+            operation = 0;
+        }
+    }
+
+    vector<int> as_operate(string instruction)
+    {
+        decode(instruction);
+        vector<int> res;
+        if (operation == 0){
+            result = 0;
+            dest = 9;
+        } else if(operation == 1){
+            result = src1 + src2;
+        } else if(operation == 2){
+            result = src1 - src2;
+        } 
+        res.push_back(dest); // 0th index of res has the DESTINATION
+        res.push_back(result); // 1th index has the result value
+        return res;
     }
 };
 
 int main()
 {
     int i;
+    vector<string> sb_mb_buf;
     string instruction;
+    ib_packet ib_pre_buf, ib_post_buf;
+    
     imemory();
     regfile();
-    decode d1;
     
+    decode d1;
+    issue issue1_2;
+    ASU as1;
+    //SIMULATION BEGIN
+    ib_pre_buf = d1.decode_to_ib(1);
+    if(DEBUG > 2){
+        cout << "=========" << endl;
+        cout << "ib_instruction : " << ib_pre_buf.ib_instruction << endl;
+    }
+
+    ib_post_buf = ib_pre_buf; //buffer transition
+
+    sb_mb_buf = issue1_2.ib_2_mb_and_sb(ib_post_buf);
+    if(DEBUG > 2){
+        cout << "sb: " << sb_mb_buf[0] << endl;
+        cout << "mb: " << sb_mb_buf[1] << endl;
+    }
+    
+    as1.as_operate(sb_mb_buf[0]);
     return 0;
 }
 
@@ -160,6 +291,8 @@ void regfile()
         }
     }
 }
+
+
 
 int atoint (string str)
 {
